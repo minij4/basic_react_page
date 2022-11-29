a1be743552b4b8d99bd3c48880c066fa84dbe73a
minij4 <emilijaskirmantaite@gmail.com>
2022-11-29 20:53:16 +0200

    Added content and styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,17 @@ import React from "react";
 
 export default function Main() {
     return(
-        <h1>Main goes here</h1>
+        <main className="main">
+            <div className="main-content">
+                <h1 className="title">Fun facts about React</h1>
+                <ul className="list">
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100K stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
+        </main>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
 import React from "react";
-
+import Logo from "../photos/logo.png";
 
 export default function Navbar() {
     return(
-        <h1>Navbar goes here</h1>
+        <nav className="nav">
+            <div className="nav-logo">
+                <img src={Logo} className="nav-logo-image" />
+                <h2 className="nav-logo-text">ReactFacts</h2>
+            </div>
+            <div>
+                <h3 className="nav-title">React Course - Project 1</h3>
+            </div>
+        </nav>
     )
 }
